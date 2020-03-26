89d097186610bc930777c7a7558d2095398fdbe8
ameerarx <55210011+ameerarx@users.noreply.github.com>
2020-03-26 23:22:53 +0500

    folder

---
+++ tg_code/folder.py
@@ -0,0 +1,16 @@
+import os 
+
+# Получить ваш текущий рабочий каталог
+os.getcwd()
+
+# Изменить текущий рабочий каталог
+os.chdir("C:\ANY_PATH")
+
+# Удалить любой каталог
+os.rmdir("FOLDERNAME")
+
+# Создать новый каталог
+os.mkdir("NEWFOLDER")
+
+# Переименовать любой каталог
+os.rename("NEWFOLDER","MYFOLDER")
