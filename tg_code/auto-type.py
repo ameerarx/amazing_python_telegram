fecde8b24e42806ba5e3008c656a5be3cce3fb02
ameerarx <55210011+ameerarx@users.noreply.github.com>
2020-04-08 17:59:23 +0500

    auto-type

---
+++ tg_code/auto-type.py
@@ -0,0 +1,9 @@
+# pip3 install pyautogui
+import pyautogui
+pyautogui.typewrite("Hello World")
+# распечатать "Hello World" мгновенно
+
+
+pyautogui.typewrite(" Hello World",interval=0.5)
+"""Распечатайте "Hello World" с задержкой 
+в полсекунды после каждого символа"""
