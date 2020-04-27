6082b8d9401a8d7976fef35f2ed79fd210b69293
ameerarx <55210011+ameerarx@users.noreply.github.com>
2020-04-27 07:10:19 +0500

    weather

---
+++ tg_code/weather.py
@@ -0,0 +1,11 @@
+# @pythonamazing(Telegram)
+import requests
+from bs4 import BeautifulSoup
+
+search = "Погода в Москве"
+url = f"https://www.google.com/search?&q={search}"
+
+r = requests.get(url)
+s = BeautifulSoup(r.text,"html.parser")
+update = s.find("div",class_="BNeawe").text 
+print(update)
