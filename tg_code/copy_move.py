a8488d73426772baa00ad5e8ff9113d65d1e6912
ameerarx <55210011+ameerarx@users.noreply.github.com>
2020-07-11 15:43:15 +0500

    copy

---
+++ tg_code/copy_move.py
@@ -0,0 +1,8 @@
+# @pythonamazing(Telegram)
+# Копировать Файлы и Папки с использованием модуля Shutil
+import shutil 
+shutil.copy(r"hello.txt",r"C:\New folder")
+
+# Перемещать файлы и папки
+import shutil
+shutil.move(r"hello.txt",r"C:\New folder")
